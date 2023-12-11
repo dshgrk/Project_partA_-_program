4fa63ac9ebbacd008e3e7c6f825b525c4501f79d
Dasha <dshgrk@gmail.com>
2023-12-12 00:46:28 +0200

    Додала клас Procedure

---
+++ partA/Procedure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partA
+{
+    public class Procedure
+    {
+        public int Price { get; set; }
+        public ProcedureTypes Name { get; set; }
+        public TimeSpan Duration { get; set; }
+        public Procedure(ProcedureTypes name, int price, duration TimeSpan) 
+        {
+            throw new NotImplementedException();
+        }
+        public void PrintToDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
