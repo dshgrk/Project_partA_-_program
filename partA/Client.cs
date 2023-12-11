323dba5b4c9b1ae1a71cce9025a78edc92f5f87c
Dasha <dshgrk@gmail.com>
2023-12-12 00:22:12 +0200

    Додала клас Client

---
+++ partA/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partA
+{
+    public class Client : IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public void DisplayInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
