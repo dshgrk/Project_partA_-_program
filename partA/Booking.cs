d045532bdd1da8ea49da5b920d59c0305ef19e98
Dasha <dshgrk@gmail.com>
2023-12-12 10:10:47 +0200

    Додала клас Booking

---
+++ partA/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partA
+{
+    public class Booking
+    {
+        public BeautySpecialist BeautySpecialist { get; set; }
+        public Client Client { get; set; }
+        public DateTime BookingTime { get; set; }
+        public Procedure Procedure { get; set; }
+
+        public Booking(Client client, Procedure procedure, DateTime bookingTime, BeautySpecialist beautySpecialist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
